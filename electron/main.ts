76f968caff6af8f482533dc603824d2e96597827
asallh <ajaypalsallh@gmail.com>
2026-03-14 17:31:40 -0600

    updating the images

--- electron/main.ts
+++ electron/main.ts
@@ -31,7 +31,7 @@ function createWindow() {
     height: 800,
     minWidth: 900,
     minHeight: 600,
-    icon: path.join(process.env.VITE_PUBLIC, 'app.png'),
+    icon: path.join(process.env.VITE_PUBLIC, 'main_icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
     },
